4d162df16ce5679d3a9accf774a4d52ee9bec39f
Sergey Kozmin <sergey.kozmin@gmail.com>
2026-01-18 22:45:41 -0800

    * update vite api base url * remove obfuscation temp

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,12 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    minify: false, // temporarily for debugging
+  },
 });
